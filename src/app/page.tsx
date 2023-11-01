0e886d8be3a1bfdce2e00c7c8a74b00aa9550d3f
tengkuhafidz <tengkuhafidz@gmail.com>
2023-11-01 21:18:47 +0800

    fix long press to download method

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,49 @@
 'use client'
+import { toPng } from 'html-to-image';
 import { useRef, useState } from "react";
-import html2canvas from 'html2canvas';
-
+import download from "downloadjs";
+import Image from "next/image"
 
 
 export default function Home() {
   const [userImage, setUserImage] = useState(null);
-  const [generatedImageUrl, setGeneratedImageUrl] = useState<string>()
-  const [showPrompt, setShowPrompt] = useState(false)
   const myComponentRef = useRef<HTMLDivElement | null>(null);
+  const downloadRef = useRef<HTMLDivElement | null>(null);
 
-  const handleImageUpload = async (e: any) => {
-    setShowPrompt(false)
+  const [hasClickedDownload, setHasClickedDownload] = useState(false)
+  const [generatedImageUrl, setGeneratedImageUrl] = useState<string>()
+
+  const handleImageUpload = (e: any) => {
     setGeneratedImageUrl(undefined)
     const file = e.target.files[0];
     const reader = new FileReader();
 
     reader.onload = (event: any) => {
       setUserImage(event.target.result);
+      setTimeout(() => {
+        generateImage()
+      }, 500)
     };
 
     reader.readAsDataURL(file);
   };
 
   const handleUploadButtonClick = () => {
+    setHasClickedDownload(false)
     document.getElementById('fileInput')?.click();
   };
 
+  const generateImage = async () => {
+    const image = await toPng(myComponentRef.current as HTMLElement)
+    setGeneratedImageUrl(image);
+    return image
+  }
 
   const handleDownload = async () => {
-    setShowPrompt(true)
-    const elementToCapture = myComponentRef.current;
-    if (elementToCapture) {
-      const canvas = await html2canvas(elementToCapture, {
-        backgroundColor: null, // Set background color to null for transparency
-      })
-      const imageUrl = canvas.toDataURL('image/png');
-      setGeneratedImageUrl(imageUrl)
-      const a = document.createElement('a');
-      a.href = generatedImageUrl as string;
-      a.download = 'profile-picture.png';
-      a.click();
+    setHasClickedDownload(true)
+    if (generatedImageUrl) {
+      download(generatedImageUrl, "profile-pic.png")
     }
-
   };
 
   return (
@@ -55,20 +56,21 @@ export default function Home() {
             <img id="borderImage" src={generatedImageUrl ?? "/bg.png"} style={{ position: 'absolute', width: '100%', height: '100%' }} className="rounded-full" />
             {!generatedImageUrl && <img id="userImage" alt='profile-image' src={userImage ?? "/user.jpg"} style={{ position: 'absolute', width: '85%', height: '85%', borderRadius: '50%', left: '7.5%', top: '7.5%' }} className="border object-cover cursor-pointer" onClick={handleUploadButtonClick} />}
           </div>
-          {showPrompt && (
+          {hasClickedDownload && (
             <div className='border p-4 rounded-lg bg-yellow-200 my-2'>
-              <p className='mt-1'>If download fails, long-press on the image to save it manually 🙏</p>
+              <p>If download fails, long press on the image to manually save it 🙏</p>
             </div>
           )}
         </div>
-        <div>
+        <div>{generatedImageUrl && (
+          <button onClick={handleDownload} className="rounded-full mb-2 py-4 px-2 w-full border border-gray-900 bg-gray-900 text-white text-xl">
+            Download Image
+          </button>
+
+        )}
           <input type="file" accept="image/*" onChange={handleImageUpload} className="hidden" id="fileInput" />
           <button onClick={handleUploadButtonClick} className="rounded-full my-2 py-4 px-2 w-full border border-gray-900 text-xl">
             Upload Image
-          </button>
-          <br />
-          <button onClick={handleDownload} className="rounded-full my-2 py-4 px-2 w-full border border-gray-900 bg-gray-900 text-white text-xl">
-            Download Image
           </button>
         </div>
         <div className='pt-8'>
