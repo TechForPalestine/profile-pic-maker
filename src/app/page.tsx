644c839125851cf29a19e6bf592e4becab844d37
tengkuhafidz <tengkuhafidz@gmail.com>
2023-11-05 12:27:32 +0800

    detect unsupportedBrowser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 'use client'
 import download from 'downloadjs';
 import { toPng } from 'html-to-image';
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const ref = useRef<HTMLDivElement>(null)
   const [userImageUrl, setUserImageUrl] = useState<string>();
-  const [hasClickedDownload, setHasClickedDownload] = useState(false)
+  const [unsuportedBrowser, setUnsupportedBrowser] = useState(false)
+
+  useEffect(() => {
+    const isInstagramBrowser = /Instagram/i.test(navigator.userAgent);
+    const isFacebookBrowser = /FBAN|FBAV/i.test(navigator.userAgent);
+
+    if (isInstagramBrowser || isFacebookBrowser) {
+      setUnsupportedBrowser(true)
+    }
+  }, [unsuportedBrowser])
 
   const handleImageUpload = (e: any) => {
     const file = e.target.files[0];
@@ -20,7 +29,6 @@ export default function Home() {
   };
 
   const handleUploadButtonClick = () => {
-    setHasClickedDownload(false)
     document.getElementById('fileInput')?.click();
   };
 
@@ -33,21 +41,23 @@ export default function Home() {
   }
 
   const handleDownload = async () => {
-    await generateImage()
-    await generateImage()
-    await generateImage()
     const generatedImageUrl = await generateImage()
     if (generatedImageUrl) {
       download(generatedImageUrl, "profile-pic.png")
     }
-    setHasClickedDownload(true)
   };
 
   return (
     <main className='text-center px-8 py-12 max-w-lg mx-auto flex justify-center align-center items-center min-h-screen'>
       <div>
-        <h1 className='font-semibold text-3xl'>Support Palestine 🇵🇸</h1>
-        <p className="text-lg py-2">Create your Palestine profile picture to show your support </p>
+        {unsuportedBrowser && (
+          <div className='border p-4 rounded-lg bg-yellow-200 my-2'>
+            <p>Downloads might not be supported on some in-app browsers. 😅</p>
+            <p>Please try on regular browsers such as Chrome if download fails 🙏</p>
+          </div>
+        )}
+        <h1 className='font-semibold text-3xl'>Show Your Support 🇵🇸</h1>
+        <p className="text-lg py-2">Create your Palestine Profile picture to spread the awareness.</p>
         <div className="my-12">
           <div className='flex justify-center'>
             <div style={{ width: '300px', height: '300px' }} className="relative" ref={ref}>
@@ -55,17 +65,12 @@ export default function Home() {
               <img id="userImage" alt='profile-image' src={userImageUrl ?? "/user.jpg"} style={{ position: 'absolute', width: '85%', height: '85%', left: '7.5%', top: '7.5%' }} className="border object-cover rounded-full cursor-pointer" />
             </div>
           </div>
-          {hasClickedDownload && (
-            <div className='border p-4 rounded-lg bg-yellow-200 my-2'>
-              <p>Please try on Chrome browser if download fails 😅</p>
-            </div>
-          )}
+
         </div>
         <div>{userImageUrl && (
           <button onClick={handleDownload} className="rounded-full mb-2 py-4 px-2 w-full border border-gray-900 bg-gray-900 text-white text-xl">
             Download Image
           </button>
-
         )}
           <input type="file" accept="image/*" onChange={handleImageUpload} className="hidden" id="fileInput" />
           <button onClick={async () => await handleUploadButtonClick()} className="rounded-full my-2 py-4 px-2 w-full border border-gray-900 text-xl">
@@ -73,6 +78,8 @@ export default function Home() {
           </button>
         </div>
         <div className='pt-8'>
+          <p className="text-lg p-2 my-6 text-sm border rounded-lg">Note: This app runs purely on your browser end. No images nor data will be saved by the app.</p>
+
           <p className='text-gray-600'>Have any feedback? <a href='https://www.instagram.com/tengkuhafidz' target='_blank' className='underline cursor-pointer'>Let me know!</a></p>
         </div>
       </div>
