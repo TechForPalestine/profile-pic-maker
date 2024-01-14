17b2dee2c8ab7fb905f0aa461de35f12b8576ee4
Umair Jibran <me@umairjibran.com>
2024-01-14 14:39:00 +0500

    Revert "remove the multi img generation"
    This reverts commit 9b5915f02ff86450a4ebaddf8091cd17a8118da6.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,6 +64,10 @@ export default function Home() {
   }
 
   const handleDownload = async () => {
+    // TODO: Fix if possible. This is a hack to ensure that image generated is as expected. Without repeating generateImage(), at times, the image wont be generated correctly. 
+    await generateImage()
+    await generateImage()
+    await generateImage()
     const generatedImageUrl = await generateImage()
     if (generatedImageUrl) {
       download(generatedImageUrl, "profile-pic.png")
