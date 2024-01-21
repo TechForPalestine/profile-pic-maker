d771f96d6e8f84ef8c74fe935bd1a6fd7c6b0157
tengkuhafidz <tengkuhafidz@gmail.com>
2024-01-21 18:42:18 +0800

    fix: revalidate api calls for latest gaza data

--- src/app/api/gaza-status/route.ts
+++ src/app/api/gaza-status/route.ts
@@ -5,12 +5,12 @@ import abbreviate from 'number-abbreviate';
 
 export const runtime = 'edge';
 export async function GET(request: NextRequest) {
-    const res = await fetch('https://data.techforpalestine.org/api/v1/summary.json')
+    const res = await fetch('https://data.techforpalestine.org/api/v2/summary.json', { next: { revalidate: 3600 } })
     if (!res.ok) {
         throw new Error('Failed to fetch data')
     }
-    const { martyred, lastDailyUpdate } = await res.json()
+    const { killed, lastDailyUpdate } = await res.json()
     const daysOfWarCrimes = dayjs(lastDailyUpdate).diff('2023-10-07', 'day')
 
-    return NextResponse.json({ summary: `${abbreviate(martyred.total)}+ killed in ${daysOfWarCrimes} days` }, { status: 200 });
+    return NextResponse.json({ summary: `${abbreviate(killed.total)} killed in ${daysOfWarCrimes} days` }, { status: 200 })
 }
